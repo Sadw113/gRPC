83b7577d79c39637209f9402f0dbd4b25de630ad
sadw113 <ww_wcw133141@mail.ru>
2025-04-03 11:19:45 +0300

    need to to do tests

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -1,1 +1,29 @@
 package service
+
+import (
+	sso "gRPC/gRPC/genGo"
+	"gRPC/internal/repo/mocks"
+	"testing"
+)
+
+func TestRegister(t *testing.T) {
+	type testTable struct {
+		repo mocks.Repository
+	}
+
+	tests := []struct {
+		name        string
+		args        testTable
+		expectedRes sso.RegisterResponse
+	}{
+		{
+			name:        "ok",
+			expectedRes: sso.RegisterResponse{Message: "Creating a new user with id 1 was successfull"},
+		},
+	}
+	for _, cases := range tests {
+		t.Run(cases.name, func(t *testing.T) {
+
+		})
+	}
+}
